6e8a0350d185593b072f1ac6d081b8343d7b9e90
DIP2202 <dip22febsaha@gmail.com>
2019-11-12 00:44:16 +0530

    STYLE

--- app/src/main/java/com/dip/flickr/PhotoDetailsActivity.java
+++ app/src/main/java/com/dip/flickr/PhotoDetailsActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PhotoDetailsActivity extends BaseActivity {
+
+    TextView txtPhotoTitle, txtAuthor, txtTags;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +26,25 @@ public class PhotoDetailsActivity extends BaseActivity {
 
         activateToolbar(true);
 
-        Intent intentFromTap = getIntent();
-        //Bundle bundle = intentFromTap.getExtras();
+        txtPhotoTitle = findViewById(R.id.txtTitle_photoDetails);
+        txtAuthor = findViewById(R.id.txtAuthor_photoDetails);
+        txtTags = findViewById(R.id.txtTags_photoDetails);
+        imageView = findViewById(R.id.imgPhoto_photoDetails);
 
-        //Photo photo = bundle.get
+        Intent intentFromTap = getIntent();
+        Photo photo = (Photo) intentFromTap.getSerializableExtra(PHOTO_TRANSFER);
+
+        if (photo != null){
+            txtPhotoTitle.setText(photo.getTitle());
+            txtTags.setText(photo.getTags());
+            txtAuthor.setText(photo.getAuthor());
+
+            Picasso.with(this).load(photo.getLink())
+                    .error(R.drawable.place_holder)
+                    .placeholder(R.drawable.place_holder)
+                    .into(imageView);
+        }
+
 
     }
 
